d06de3d00e15c5536160e5657c05eb1d9951eb73
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2023-07-17 18:58:14 +0200

    Created simple tika extractor.

---
+++ src/vectorization/VectorizationSample/VectorizationSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using Serilog;
+
+namespace VectorizationSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Console.WriteLine("Hello, World!");
+
+            TikaAnalyzer analyzer = new TikaAnalyzer(
+                @"C:\Program Files\Amazon Corretto\jdk18.0.2_9\bin\java.exe",
+                Environment.GetEnvironmentVariable("TIKA_HOME"));
+
+            var extracted = analyzer.GetHtmlContent(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "docs", "sample.pdf"));
+
+            //Parse HTML with agility pack
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(extracted);
+            var pages = htmlDoc.DocumentNode.SelectNodes("//div[@class='page']");
+            var allPages = pages.ToList();
+            Console.WriteLine("We have {0} pages", allPages.Count);
+            foreach (var page in allPages)
+            {
+                var pageText = page.InnerText;
+                Console.WriteLine(pageText);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/vectorization/VectorizationSample/VectorizationSample/TikaExtractor.cs
@@ -0,0 +1,77 @@
+﻿using Serilog;
+using System.Diagnostics;
+
+namespace VectorizationSample
+{
+    public class TikaAnalyzer
+    {
+        private string? _tikaLocation;
+        private string _pathToJavaExe;
+
+        public TikaAnalyzer(string javaExePath, string tikaJarPath)
+        {
+            _tikaLocation = tikaJarPath;
+            _pathToJavaExe = javaExePath;
+            if (!File.Exists(_tikaLocation))
+            {
+                throw new Exception(string.Format("Tika not found on {0}", _tikaLocation));
+            }
+            if (!File.Exists(_pathToJavaExe))
+            {
+                throw new Exception(string.Format("Java not found on {0}", _pathToJavaExe));
+            }
+        }
+
+        public string GetHtmlContent(string pathToInputFile)
+        {
+            try
+            {
+                var arguments = String.Format("-jar {0} -h \"{1}\"", _tikaLocation, pathToInputFile);
+
+                Log.Debug("Executing {0} {1}", _pathToJavaExe, arguments);
+
+                var psi = new ProcessStartInfo(_pathToJavaExe, arguments)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Minimized
+                };
+
+                string content = null;
+                using (var p = Process.Start(psi))
+                {
+                    //wait for 30 seconds with timeout, if extraction requires more than 30 seconds file is really too big or problematic
+                    var closedCorrectly = p.WaitForExit(1000 * 30);
+                    if (!closedCorrectly)
+                    {
+                        //we have a timeout, ok we really need to kill the process and consider impossible to extract text from this file
+                        p.Kill();
+                        return "";
+                    }
+
+                    using (var reader = p.StandardOutput)
+                    {
+                        content = reader.ReadToEnd();
+                    }
+
+                    if (p.ExitCode == 0)
+                    {
+                        return content;
+                    }
+                    else
+                    {
+                        Log.Error("failed extracting with {0} exit code {1}", _tikaLocation, p.ExitCode);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Log.Error("Error extracting with tika {0}", _tikaLocation);
+            }
+
+            return "";
+        }
+    }
+}
