94f0a44ecad8516d33b0295104cd4b68182116ff
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2023-08-16 12:11:51 +0200

    Added vectorizer in flask.

---
+++ src/python/langchainVarious/vectorization/vectorservice.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from sentence_transformers import SentenceTransformer
+import os
+
+app = Flask(__name__)
+
+transformers_cache = os.environ.get('TRANSFORMERS_CACHE')
+transformers_dict = {}
+
+@app.route('/vectorize', methods=['POST'])
+def vectorize_text():
+    data = request.get_json()
+    model_name = data['Model']
+    text = [data['Text']]
+    
+    # Check if model already exists in dictionary
+    if model_name in transformers_dict:
+        model = transformers_dict[model_name]
+    else:
+        # Create new model and add to dictionary
+        model = SentenceTransformer(
+            model_name, 
+            cache_folder=transformers_cache,
+            device='cuda')
+        transformers_dict[model_name] = model
+
+    encoded = model.encode(text)
+
+    response = {'vector': encoded.tolist()}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5001)
