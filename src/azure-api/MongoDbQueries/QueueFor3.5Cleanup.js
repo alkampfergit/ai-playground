02e8047599eb3b196904e97c8bfeacf35c938ad7
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2023-08-18 13:37:12 +0200

    Further explanation of langchain

--- src/azure-api/MongoDbQueries/QueueFor3.5Cleanup.js
+++ src/azure-api/MongoDbQueries/QueueFor3.5Cleanup.js
@@ -1,13 +1,13 @@
-db.getCollection('documents_to_index').updateMany(
+db.getCollection('documents_to_index').updateOne(
 {
-     "Pages.Gpt35PageInformation.CleanText" : {$exists : 0}
+     "Pages.Gpt35PageInformation.CleanText" : {$exists : 0}
     //"_id" : "S:\\OneDrive\\Documentation\\HumbleBundle\\Interesting\\advancedapisecurity.pdf"
   },
   { 
     $unset : {"Processing" : ""},
     $set : { "CleanWithGpt35" : ISODate("2023-08-01T12:26:53.946Z")} 
 })
-
-
-// to remove everything from the queue
+
+
+// to remove everything from the queue
 //db.getCollection('documents_to_index').updateMany({}, {$unset : {"CleanWithGpt35" : "","Processing" : "" }})
