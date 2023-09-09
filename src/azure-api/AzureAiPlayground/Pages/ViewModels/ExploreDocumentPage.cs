88faccac80df63ac593be19f93897b6f1eaa8365
gian maria ricci <ricci.gianmaria@gmail.com>
2023-09-09 12:57:25 +0200

    More on single doc analysis

--- src/azure-api/AzureAiPlayground/Pages/ViewModels/ExploreDocumentPage.cs
+++ src/azure-api/AzureAiPlayground/Pages/ViewModels/ExploreDocumentPage.cs
@@ -5,17 +5,39 @@ namespace AzureAiPlayground.Pages.ViewModels;
 
 public class ExploreDocumentPage : ComponentBase
 {
-    public string? FilePathToImport { get; set; }
-
-    public UiSingleDocument? CurrentDocument { get; set; }
+    public string? FilePathToImport { get; set; } = "/Users/gianmariaricci/Downloads/advancedapisecurity.pdf";
     
-    [Inject] public ExploreDocumentViewModel DocumentsViewModel { get; set; } = null!;
+    [Inject] public ExploreDocumentViewModel ViewModel { get; set; } = null!;
+
+    public MudTable<UiSingleDocumentPage> ResultTable { get; set; } = null!;
+
+    public UiSingleDocumentPage SelectedPage { get; set; }
 
     public async Task ExtractDocument()
     {
         if (String.IsNullOrEmpty(FilePathToImport)) return;
 
         var id = System.IO.Path.GetFileName(FilePathToImport);
-        CurrentDocument = await DocumentsViewModel.ExtractDocument(FilePathToImport!, id);
+        await ViewModel.ExtractDocument(FilePathToImport!, id);
+    }
+    
+    public string SelectedRowClassFunc(UiSingleDocumentPage item, int rowNumber)
+    {
+        if (item == SelectedPage)
+        {
+            return "selected";
+        }
+
+        return string.Empty;
+    }
+
+    public void RowClickEvent(TableRowClickEventArgs<UiSingleDocumentPage> evt)
+    {
+        SelectedPage = evt.Item;
+    }
+    
+    public void ShowBtnPress(UiSingleDocumentPage page)
+    {
+        page.ShowDetails = !page.ShowDetails;
     }
 }
--- src/azure-api/AzureAiPlayground/Pages/ViewModels/ExploreDocumentViewModel.cs
+++ src/azure-api/AzureAiPlayground/Pages/ViewModels/ExploreDocumentViewModel.cs
@@ -6,19 +6,18 @@ using AzureAiLibrary.Documents.Support;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using TiktokenSharp;
 using static AzureAiLibrary.Documents.ElasticSearchService;
 
 namespace AzureAiPlayground.Pages.ViewModels;
 
 public class ExploreDocumentViewModel
 {
-    private readonly Gpt35AiCleaner _gpt35AiCleaner;
-    private readonly ElasticSearchService _elasticSearchService;
-    private readonly ElasticSearchIndexerJob _elasticIndexer;
-    private readonly PythonTokenizer _pythonTokenizer;
     private IMongoDatabase _mongoDatabase;
     private TikaOutOfProcess _tikaOutOfProcess;
-    private RawDocumentSimpleReader _rawDocumentSimpleReader;
+    private readonly IMongoCollection<SingleDocument> _docCollection;
+    private readonly IMongoCollection<SingleDocumentPage> _pagesCollection;
+    private readonly TikToken _tikTokTokenizer;
 
     public ExploreDocumentViewModel(
         IOptionsMonitor<DocumentsConfig> documentsConfig,
@@ -29,21 +28,24 @@ public class ExploreDocumentViewModel
         var settings = MongoClientSettings.FromUrl(url);
         var client = new MongoClient(settings);
         _mongoDatabase = client.GetDatabase("DocSampleV1");
+
+        _docCollection = _mongoDatabase.GetCollection<SingleDocument>("SingleDocument");
+        _pagesCollection = _mongoDatabase.GetCollection<SingleDocumentPage>("SingleDocumentPages");
+        
+        // check we can access the database reading collection 
+        var count = _docCollection.CountDocuments(new BsonDocument());
+        
         // Need to use Tika to extract document.
         _tikaOutOfProcess = 
             new TikaOutOfProcess(
                 documentsConfig.CurrentValue.JavaBin,
                 documentsConfig.CurrentValue.Tika);
-
-        //TODO: Register database and register this in the ui container.
-        _rawDocumentSimpleReader = new RawDocumentSimpleReader(_mongoDatabase);
-
-        //TODO register all jobs and resolve all
-        _gpt35AiCleaner = new Gpt35AiCleaner(_mongoDatabase, chatClient);
-        _elasticSearchService = new ElasticSearchService(new Uri(documentsConfig.CurrentValue.ElasticUrl));
-        _elasticIndexer = new ElasticSearchIndexerJob(_mongoDatabase, _elasticSearchService);
-        _pythonTokenizer = pythonTokenizer;
+        
+        TikToken.PBEFileDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+        _tikTokTokenizer = TikToken.GetEncoding("cl100k_base");
     }
+    
+    public UiSingleDocument? CurrentDocument { get; set; } 
     
     /// <summary>
     /// Uses tika to extract document metadata and document pages from a single document, it will return
@@ -52,39 +54,109 @@ public class ExploreDocumentViewModel
     /// <param name="path"></param>
     /// <param name="docId"></param>
     /// <returns></returns>
-    public async Task<UiSingleDocument> ExtractDocument(string path, string docId)
+    public async Task ExtractDocument(string path, string docId)
+    {
+        //try to load the document from mongo if it is not present, lets tika extract it
+        //the id is docid
+        var doc = _docCollection.Find(x => x.Id == docId).FirstOrDefault();
+        if (doc != null)
+        {
+            //load all the pages from mongo
+            var loadedPages = _pagesCollection.Find(x => x.SingleDocumentId == docId).ToList();
+            CurrentDocument = new UiSingleDocument(doc, loadedPages);
+            return;
+        }
+
+        await GetDocumentFromFileWithTikaAsync(docId, path);
+    }
+
+    public async Task UpdateCountToken()
+    {
+        if (CurrentDocument == null) return;
+
+        foreach (var page in CurrentDocument.Pages)
+        {
+            page.Page.TokenCount = _tikTokTokenizer.Encode(page.Page.Content).Count;
+        }
+        await SaveCurrentDocumentInMongoDb();
+    }
+
+    private async Task SaveCurrentDocumentInMongoDb()
+    {
+        if (CurrentDocument == null) return;
+        
+        await SaveDocumentAsync(CurrentDocument.Document, CurrentDocument.Pages.Select(p => p.Page));
+    }
+
+    private async Task GetDocumentFromFileWithTikaAsync(string docId, string path)
     {
         var extractedData = await _tikaOutOfProcess.GetHtmlContentAsync(path);
+        if (!extractedData.Success) return; // TODO: communicate error
         var singleDocument = new SingleDocument
         {
             Id = docId,
             Metadata = new Dictionary<string, IReadOnlyCollection<string>>()
         };
-        foreach (var kvp in extractedData.Metadata)
+        foreach (var kvp in extractedData.Metadata!)
         {
             singleDocument.Metadata.Add(kvp.Key, kvp.Value);
         }
+
         List<SingleDocumentPage> pages = new List<SingleDocumentPage>();
-        for (var page = 0; page < extractedData.Pages.Count; page++)
+        for (var page = 0; page < extractedData.Pages!.Count; page++)
         {
-            var documentPage = new SingleDocumentPage(docId,page, extractedData.Pages.ElementAt(page));
+            var documentPage = new SingleDocumentPage(docId, page, extractedData.Pages.ElementAt(page));
             pages.Add(documentPage);
         }
-        return new UiSingleDocument(singleDocument, pages);
+
+        CurrentDocument = new UiSingleDocument(singleDocument, pages);
+        await SaveDocumentAsync(singleDocument, pages);
+    }
+
+    private async Task SaveDocumentAsync(SingleDocument doc, IEnumerable<SingleDocumentPage> pages)
+    {
+        // Save the document and all the pages replacing all previous data in mongodb
+        await _docCollection.ReplaceOneAsync(
+            x => x.Id == doc.Id,
+            doc,
+            new ReplaceOptions
+            {
+                IsUpsert = true
+            });
+        
+        // Save all the pages in a single bulk operation, first delete already existing
+        // pages related to this document then save in bulk all the pages
+        await _pagesCollection.DeleteManyAsync(x => x.SingleDocumentId == doc.Id);
+        await _pagesCollection.InsertManyAsync(pages);
     }
 }
 
 public class UiSingleDocument
 {
-    public UiSingleDocument(SingleDocument document, IReadOnlyCollection<SingleDocumentPage> pages)
+    public UiSingleDocument(SingleDocument document, IEnumerable<SingleDocumentPage> pages)
     {
         Document = document;
-        Pages = pages;
+        Pages = pages
+            .OrderBy(p => p.PageNumber)
+            .Select(p => new UiSingleDocumentPage(p))
+            .ToArray();
     }
 
     public SingleDocument Document { get; set; }
     
-    public IReadOnlyCollection<SingleDocumentPage> Pages { get; set; }
+    public IReadOnlyCollection<UiSingleDocumentPage> Pages { get; set; }
+}
+
+public class UiSingleDocumentPage
+{
+    public UiSingleDocumentPage(SingleDocumentPage page)
+    {
+        Page = page;
+    }
+
+    public bool ShowDetails { get; set; } = false;
+
+    public SingleDocumentPage Page { get; set; }
 }
 
 /// <summary>
@@ -119,4 +191,6 @@ public class SingleDocumentPage
     public string SingleDocumentId { get; set; }
 
     public string Content { get; set; }
+
+    public int? TokenCount { get; set; }
 }
